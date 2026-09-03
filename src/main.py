1a94bd1fc3e082313d61de77e9d2a3dd5d05610e
markusens8 <198192158+markusens8@users.noreply.github.com>
2026-09-03 15:38:26 +0300

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from random import randint
+﻿from random import randint
 
 # viz lietas
 import viz
@@ -14,32 +14,54 @@ def generatePosition():
 	z = randint(-PLATFORM_LENGTH, PLATFORM_LENGTH)/2
 	return [x, y, z]
 
-viz.go()
-viz.mouse.setVisible(viz.OFF)
-viz.clearcolor(1,0,0)
+def startMenu():
+	title = viz.addText('Cau burvi!', parent=viz.SCREEN, pos=(0.5, 0.9, 0), fontSize=50)
+	title.alignment(viz.ALIGN_CENTER_CENTER)
 
-navigator = vizcam.WalkNavigate(moveScale = 20, turnScale = 0.8)
-viz.cam.setHandler(navigator)
-viz.MainView.setPosition(0, 1.5, 0)  # Iestata sākuma pozīciju
-viz.MainView.setEuler(0, 0, 0)  # Iestata sākuma skatiena virzienu
+	press = viz.addText('spied ENTER lai turpinātu!', parent=viz.SCREEN, pos=(0.5, 0.5, 0), fontSize=50)
+	press.alignment(viz.ALIGN_CENTER_CENTER)
 
-# tekst
-text = viz.addText('Cau burvi!', parent=viz.SCREEN, pos=(0.5, 0.9, 0))
-text.fontSize(54)
-text.color(viz.YELLOW)
+def gameLoop():
+	navigator = vizcam.WalkNavigate(moveScale = 30, turnScale = 0.8)
+	viz.cam.setHandler(navigator)
+	viz.MainView.collision(viz.ON)
+	viz.MainView.setPosition(0, 100, 0)  # Iestata sākuma pozīciju
+	viz.MainView.setEuler(0, 0, 0)  # Iestata sākuma skatiena virzienu
 
-# grida
-floor = vizshape.addPlane(size=(PLATFORM_WIDTH, PLATFORM_LENGTH), color=(0, 1, 0))
-floor.setPosition(0, 0, 0)
-floor.disable(viz.LIGHTING)
-
-# genere bumbas
-for i in range(50):
-	ball = vizshape.addSphere()
-	position = generatePosition()
-	ball.setPosition(position[0], position[1], position[2])
+	# tekst
+	cau = viz.addText('Cau burvi!', parent=viz.SCREEN, pos=(0.75, 0.9, 0))
+	cau.alignment(viz.ALIGN_CENTER_CENTER)
+	cau.fontSize(54)
+	cau.color(viz.YELLOW)
 	
+	punkti = viz.addText('punkti: ', parent=viz.SCREEN, pos=(0.25, 0.9, 0))
+	punkti.alignment(viz.ALIGN_CENTER_CENTER)
+	punkti.fontSize(54)
+	punkti.color(viz.YELLOW)
 
+	# Pievieno virziena gaismu, AKA saule
+	dir_light = viz.addDirectionalLight(euler=(0, 90, 0))
+	dir_light.intensity(1.0)
+
+	# grida
+	floor = vizshape.addPlane(size=(PLATFORM_WIDTH, PLATFORM_LENGTH), color=(0, 1, 0))
+	floor.setPosition(0, 0, 0)
+
+	# genere bumbas
+	balls = []
+	for i in range(100):
+		ball = vizshape.addSphere()
+		position = generatePosition()
+		ball.setPosition(position[0], position[1], position[2])
+		balls.append(ball)
+
+
+if __name__ == "__main__":
+	viz.go()
+	viz.clearcolor(1,0,0)
+	viz.mouse.setVisible(viz.OFF)
+	#startMenu()
+	gameLoop()
 
 
 
